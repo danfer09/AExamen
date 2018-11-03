f291e8dec42089c0c8ac33da3d5386a63b585bef
danfer09 <daniel.fer333@gmail.com>
2018-11-03 17:56:03 +0100

    index.php y servidor.php añadido

--- servidor.php
+++ servidor.php
@@ -1,1 +1,28 @@
+<?php
+// Conectando, seleccionando la base de datos
+$link = mysql_connect('localhost', '', '')
+    or die('No se pudo conectar: ' . mysql_error());
+echo 'Connected successfully';
+mysql_select_db('my_database') or die('No se pudo seleccionar la base de datos');
 
+// Realizar una consulta MySQL
+$query = 'SELECT * FROM profesores';
+$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
+
+// Imprimir los resultados en HTML
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Liberar resultados
+mysql_free_result($result);
+
+// Cerrar la conexión
+mysql_close($link);
+?>
