f93c18b049740c3895b9e0630a02d2e8bf6a9148
danfer09 <daniel.fer333@gmail.com>
2018-12-12 12:00:01 +0100

    Todos los js descargados en local y los enlaces que los referencian modificados

--- examenes.php
+++ examenes.php
@@ -207,17 +207,10 @@
 	</div>
 
 	<script src="js/jquery-3.3.1.min.js"></script>
-	<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>-->
 	<script type="text/javascript" src="js/cabeceraConLogin.js"></script>
 	<script type="text/javascript" src="js/examenes.js"></script>
-
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	
-	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>-->
-	<script src="https://www.w3schools.com/lib/w3.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/w3.js"></script>
 
 </body>
 </html>
