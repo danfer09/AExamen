5bd20485859f1b8b9df2be88e0ca2de02012c2cb
danfer09 <daniel.fer333@gmail.com>
2019-02-01 13:19:55 +0100

    Formularios de la página 'definir parametros de exámen' hechos, no aparecen los paneles no se porque, creo que está todo bien
    Formularios de la página 'definir parametros de exámen' hechos, no aparecen los paneles no se porque, creo que está todo bien

--- definirParametrosExam.php
+++ definirParametrosExam.php
@@ -27,33 +27,111 @@
 <div class="header" id="header"></div>
 <div class="container">
 <h1>Pagina de coordinador</h1>
-	<?php
-		/*Incluimos asignaturasProfesorProcesamiento.php donde tenemos implementadas
-		algunas funciones que usaremos más adelante*/
-		include 'asignaturasProfesorProcesamiento.php'; 
-		echo "<h2> Asignaturas de ". $_SESSION['nombre']. "</h2>";
-	?>
-	<div class="border border-secondary">
 		<form>
 			<div class="row">
-				<div class="form-group col-4">
-				    <label>Tema 1:</label>
-				    <input type="number" class="form-control" id="exampleInputEmail1">
-				</div>
-				<div class="form-group col-4">
-				    <label>Tema 2:</label>
-				    <input type="number" class="form-control" id="exampleInputEmail1">
-				</div>
-				<div class="form-group col-4">
-				    <label>Tema 3:</label>
-				    <input type="number" class="form-control" id="exampleInputEmail1">
+				<div class="form-group col-11"></div>
+				<div class="form-group col-1">
+					<button type="submit" class="btn btn-primary">Guardar</button>
 				</div>
 			</div>
-			<button type="submit" class="btn btn-primary">Submit</button>
+			<div class="panel-group">
+				<!--Formulario para meter puntos por tema-->
+				<div class="panel panel-primary">
+					<div class="panel-heading"><h5>Puntos por tema</h5></div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="form-group col-4">
+							    <label>Tema 1:</label>
+							    <input type="number" class="form-control" id="">
+							</div>
+							<div class="form-group col-4">
+							    <label>Tema 2:</label>
+							    <input type="number" class="form-control" id="">
+							</div>
+							<div class="form-group col-4">
+							    <label>Tema 3:</label>
+							    <input type="number" class="form-control" id="">
+							</div>
+						</div>
+					</div>
+				</div>
+				<br>
+				<!-- Formulario para definir cabecera del examen-->
+				<div class="panel panel-primary">
+					<div class="panel-heading"><h5>Cabecera de exámen</h5></div>
+				  <div class="panel-body">
+						<div class="row">
+							<div class="form-group col-6">
+							  <div class="custom-file">
+							  	<label>Logo de la Facultad</label>
+							     <select class="form-control" id="exampleFormControlSelect1">
+							      <option>Universidad Complutense</option>
+							      <option>Facultad de Informática</option>
+							    </select>
+							  </div>
+							</div>
+							<div class="form-group col-6">
+								<div class="form-group">
+							    <label>Cuatrimestre</label>
+							    <select class="form-control" id="exampleFormControlSelect1">
+							      <option>Primer Cuatrimetre</option>
+							      <option>Segundo Cuatrimestre</option>
+							    </select>
+							  </div>
+						  </div>
+						</div>
+					</div>
+				</div>
+				<br>
+				<!-- Formulario para definir espacio entre preguntas-->
+				<div class="panel panel-primary">
+					<div class="panel-heading"><h5>Espaciado entre preguntas por defecto</h5></div>
+				  <div class="panel-body">
+						<div class="row">
+							<div class="form-group col-4">
+								<div class="form-check">
+								  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+								  <label class="form-check-label" for="exampleRadios1">
+								    Pequeño
+								  </label>
+								</div>
+							</div>
+							<div class="form-group col-4">
+								<div class="form-check">
+								  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
+								  <label class="form-check-label" for="exampleRadios1">
+								    Medio
+								  </label>
+								</div>
+							</div>
+							<div class="form-group col-4">
+								<div class="form-check">
+								  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
+								  <label class="form-check-label" for="exampleRadios1">
+								    Grande
+								  </label>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<br>
+				<!-- Formulario para definir texto de cabecera-->
+				<div class="panel panel-primary">
+					<div class="panel-heading"><h5>Texto breve inicial(consejos, normas, etc.)</h5></div>
+				  <div class="panel-body">
+						<div class="row">
+							<div class="form-group col-12">
+								<div class="form-group">
+							    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"placeholder="Escribe aquí"></textarea>
+							  </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</form>
 	</div>
-		
-</div>
 
 <!--Librerias externas-->
 <script src="js/jquery-3.3.1.min.js"></script>
