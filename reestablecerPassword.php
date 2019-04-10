3972f9fb60e89f5f3a66f497218d2f7be8715677
danfer09 <daniel.fer333@gmail.com>
2019-04-10 19:39:49 +0200

    Todas las librerias en local
    Todas las librerias en local

--- reestablecerPassword.php
+++ reestablecerPassword.php
@@ -38,9 +38,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	<!--css propio -->
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<!--css externos-->
+	<link rel="stylesheet" type="text/css" href="css/w3.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/all.css">
 	<meta charset="UTF-8">
 </head>
 <body>
